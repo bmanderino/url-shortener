51625639f71adc4f0c3b695240f082562ca5ec63
Brian Manderino <brian.manderino@gmail.com>
2025-01-10 11:19:42 -0800

    shorturl api

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
+const dns = require('dns');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +22,27 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+const options = {
+  family: 6,
+  hints: dns.ADDRCONFIG | dns.V4MAPPED,
+};
+options.all = true
+app.post('/api/shorturl', async (req, res) => {
+  let url = req.body.url
+  let testURL = url;
+  let isValidUrl = false
+  if (url.includes('://')) {
+    testURL = url.split('://')[1]
+  }
+  dns.lookup(testURL, options, (err, addresses) => {
+    if (addresses !== undefined) {
+      res.json({response: "hello"})
+    } else {
+      res.json({error: "Invalid URL"})
+    }
+  })
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
